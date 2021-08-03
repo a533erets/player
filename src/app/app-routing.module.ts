3236755039b9fdcfa343ef917f54dfd152926ed7
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2021-08-03 21:10:28 +0800

    arrange tabs icon styling and routing. Faild to go back to homepage.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,38 +4,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-    },
-  
-  {
-    path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'tab1',
-    loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule)
-  },
-  {
-    path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  {
-    path: 'order',
-    loadChildren: () => import('./order/order.module').then( m => m.OrderPageModule)
-  },
-  {
-    path: 'store-situation',
-    loadChildren: () => import('./store-situation/store-situation.module').then( m => m.StoreSituationPageModule)
-  },
-  {
-    path: 'delivery-tracking',
-    loadChildren: () => import('./delivery-tracking/delivery-tracking.module').then( m => m.DeliveryTrackingPageModule)
-  },
-  {
-    path: 'member',
-    loadChildren: () => import('./member/member.module').then( m => m.MemberPageModule)
-  }
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full'
+  //   }
 ];
 @NgModule({
   imports: [
