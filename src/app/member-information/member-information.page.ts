b9ba68f708c6da8a6a6ff2f439e86c499163f2e4
jeremy0090 <88364619+jeremy0090@users.noreply.github.com>
2021-08-07 20:01:21 +0800

    member-information

--- src/app/member-information/member-information.page.ts
+++ src/app/member-information/member-information.page.ts
@@ -11,14 +11,18 @@ export class MemberInformationPage implements OnInit {
    members:object[]=[]
    
   ngOnInit() {
+    this.getMembers()
+
+  
+  }
+  getMembers(){
     this.http.get('http://localhost/foodplayer/src/app/php/getMember.php')
 
 .subscribe(data=>{
-  console.log(data);
+  console.log(data)
   for (let i=0; i<Object.keys(data).length ;i++){
     this.members.push( data[i])
   }
 })
   }
-
 }
