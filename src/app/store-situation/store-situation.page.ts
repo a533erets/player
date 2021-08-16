62622898777d43f3ff27ac11a02ace7ddf067752
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2021-08-16 14:13:41 +0800

    global.scss: Globalize styles for advertise. Chang color when avtionBtn is pressed. home.page.html: Partially reconstruct the layout. home.page.scss: Remove unnecessary styles. advertise.page.html: Partially reconstruct the layout. advertise.page.scss: Remove unnecessary styles. main.page.html: Partially reorganize the layout. main.page.scss: Remove unnecessary styles and apply new styles. player-tabs.page.scss: Change color when the tab is selected. store-situation.page.html: Construct page layout. Binding data's from ts file store-situation.page.scss: Apply styles for seats. store-situation.page.ts: Generate seats dynamically on init and randomly decide witch one is occupied. Get current day, hour and minute on ionViewWillEnter life hook and respond with text.

--- src/app/store-situation/store-situation.page.ts
+++ src/app/store-situation/store-situation.page.ts
@@ -7,9 +7,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreSituationPage implements OnInit {
 
+  /* 營業時間: 上午 11:00 ~ 14:30   下午 17:00 ~20:30
+    禮拜二休假*/
+  toDay: any
+  seats: any = []
+  openStatus: any
+  remainSeats: any
+
   constructor() { }
 
   ngOnInit() {
+    let seats = Array.from({length: 16}, (seats ,seat)=> seat)
+    console.log(seats)
+
+    for(let i=0; i < seats.length; i++){
+      this.seats.push({seat: seats[i], occupied: this.randomSeat(), message: '空'})
+    }
+    console.log(this.seats)
   }
 
+  ionViewWillEnter(){
+    this.checkDate()
+    this.checkSeat()
+  }
+
+  checkDate(){
+    let day = new Date().getDay()
+    let hour = new Date().getHours()
+    let minute = new Date().getMinutes()
+
+    if(day === 2){
+      console.log('今日休息')
+      this.openStatus = '今日休息'
+    }
+
+    if( hour === 14 && minute < 30  || hour === 20 && minute < 30){
+      console.log('營業中')
+      this.openStatus = '營業中'      
+    }
+    else if(hour >= 11 && hour < 14  || hour > 17 && hour < 20){
+      console.log('營業中')
+      this.openStatus = '營業中'
+    }else{
+      console.log('尚未營業')
+      this.openStatus = '尚未營業'
+    }
+  }
+
+  checkSeat(){
+    let remain = 0
+    for(let i=0; i < this.seats.length; i++){
+      if(this.seats[i].occupied === false){
+        remain++
+      }else{
+        this.seats[i].message = '滿'
+      }
+    }
+    this.remainSeats = remain
+  }
+
+  randomSeat(){
+    let random = Math.random() < 0.5
+    return random
+  }
 }
