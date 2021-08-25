b33e64144569472be67dda6b7923ccf9bf03dd7b
a533erets <a533erets@gmail.com>
2021-08-25 11:08:27 +0800

    new toCart.php

--- src/app/php/toCart.php
+++ src/app/php/toCart.php
@@ -9,16 +9,18 @@ $payment = "";
 $paid = 'false';
 $today = date("Y-m-d H:i:s");
 $stars = 0;
+$bonus = '';
 
 try{
     require_once('connect.php');
-    if(isset($_POST['userName'], $_POST['userID'], $_POST['theCart'], $_POST['total'], $_POST['payment'])){
+    if(isset($_POST['userName'], $_POST['userID'], $_POST['theCart'], $_POST['total'], $_POST['payment'], $_POST['bonus'])){
         $userID = $_POST['userID'];
         $userName = $_POST['userName'];
         $theCart = $_POST['theCart'];
         $total = $_POST['total'];
         $payment = $_POST['payment'];
-        // echo $theCart;
+        $bonus = $_POST['bonus'];
+
         $newCart = $pdo -> prepare("INSERT INTO foodplayer.shopping_cart (`shopping_cart_ID`, `member_ID`, `member_name`, `product_list`, `total`, `payment_method`, `paid`, `date`, `stars`) VALUES (:shopping_cart_ID, :member_ID, :member_name, :product_list, :total, :payment_method, :paid, :date, :stars)");
         $newCart->bindValue(':shopping_cart_ID', $cartID);
         $newCart->bindValue(':member_ID', $userID);
@@ -30,9 +32,13 @@ try{
         $newCart->bindValue(':date', $today);
         $newCart->bindValue(':stars', $stars);
 
-        if($newCart -> execute()){
-            // echo 'success';
-            echo json_encode($cartID);
+        $upDateBonus = $pdo -> prepare("UPDATE foodplayer.vip SET `bonus`= bonus + :bonus WHERE `member_ID`=:member_ID");
+        $upDateBonus->bindValue(':bonus', $bonus);
+        $upDateBonus->bindValue(':member_ID', $userID);
+
+        if($newCart -> execute() AND $upDateBonus -> execute()){
+            $response = ['cartID'=> $cartID, 'bonus'=> $bonus];
+            echo json_encode($response);
         }else{
             echo 'faild';
         }
